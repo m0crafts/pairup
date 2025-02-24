8d7c355f07965c733f1eba9daa1eef35c5dd4d48
Mohamed Aly <troy100100@gmail.com>
2025-02-24 22:21:11 +0200

    Fix path issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
-import logo from "./assets/logo.png";
+import logo from "../../public/assets/logo.png";
 
 export default function Header({
   level,
